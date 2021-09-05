a29fb9b21c2e8366b5fe027627b820ec6941256f
Ekaterina Kolesnikova <1005885@mail.ru>
2021-09-05 18:34:30 +0300

    Update cleanDB

--- src/test/java/ru/netology/db/DataBase.java
+++ src/test/java/ru/netology/db/DataBase.java
@@ -12,10 +12,10 @@ public class DataBase {
         var conn = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/app", "app", "pass");
 
-        conn.prepareStatement("DELETE FROM auth_codes");
-        conn.prepareStatement("DELETE FROM card_transactions");
-        conn.prepareStatement("DELETE FROM cards");
-        conn.prepareStatement("DELETE FROM users");
+        conn.prepareStatement("DELETE FROM users").execute();
+        conn.prepareStatement("DELETE FROM cards").execute();
+        conn.prepareStatement("DELETE FROM auth_codes").execute();
+        conn.prepareStatement("DELETE FROM card_transactions").execute();
     }
 
     @SneakyThrows
